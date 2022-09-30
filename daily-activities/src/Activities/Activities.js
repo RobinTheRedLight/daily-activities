90a8766ec97085c4d85e7ceeb313a37c7041b524
RobinTheRedLight <robinmitra789@gmail.com>
2022-09-30 20:21:13 +0600

    All-card-added

--- daily-activities/src/Activities/Activities.js
+++ daily-activities/src/Activities/Activities.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActivityCalculation from '../ActivityCalculation/ActivityCalculation';
 import ActivityCard from '../ActivityCard/ActivityCard';
 import Header from '../Header/Header';
 import './Activities.css'
 
 const Activities = () => {
+    const [items, setItem] = useState([]);
+    useState(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setItem(data))
+    }, 0);
     return (
         <div className="total-activity">
             <div className='left-sec'>
                 <Header></Header>
                 <div className="daily-act">
-                    <ActivityCard></ActivityCard>
+                    {
+                        items.map(item =>
+                            <ActivityCard item={item} key={item.id}></ActivityCard>
+                        )
+                    }
                 </div>
             </div>
             <div className="activity-count">
--- daily-activities/src/ActivityCard/ActivityCard.js
+++ daily-activities/src/ActivityCard/ActivityCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ActivityCard.css'
-const ActivityCard = () => {
+const ActivityCard = ({ item }) => {
+    const { id, name, time, img } = item
     return (
         <div className="activityCard">
             <div className="card">
-                <img src="1.jpg" alt="" />
-                <h1>Tailored Jeans</h1>
-                <p className="price">$19.99</p>
-                <p><button>Add to Cart</button></p>
+                <img src={img} alt="" />
+                <h1>{name}</h1>
+                <p className="price">Time:{time}</p>
+                <p><button>Add to Activity</button></p>
             </div>
         </div>
     );
